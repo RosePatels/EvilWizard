f580fe0b68c43571d942db7352277048c9e5fbbb
Shree <shree1795@gmail.com>
2020-08-12 16:15:51 -0400

    energy ball assets and wizard assets

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,10 +4,12 @@ import background from "../assets/scifi_platform_BG1.jpg";
 import tiles from "../assets/scifi_platformTiles_32x32.png";
 import star from "../assets/star.png";
 import ball from "../assets/ball.png";
+import wizard from "../assets/wizard_idle.png";
 import { accelerate, decelerate } from "../utils";
 
 let box;
 let cursors;
+let Evilwizard;
 
 export default new Phaser.Class({
   Extends: Phaser.Scene,
@@ -27,15 +29,26 @@ export default new Phaser.Class({
       frameWidth: 100,
       frameHeight: 100
     });
+
+    this.load.spritesheet("wizard", wizard, {
+      frameWidth: 150,
+      frameHeight: 195,
+    });
   },
   create: function create() {
     this.add.image(400, 300, "background");
 
+    Evilwizard = this.physics.add.image(400, 100, "wizard");
+
+    Evilwizard.scaleX = .5;
+    Evilwizard.scaleY = .5;
+
+
     const balls = this.physics.add.group({
       key: 'ball',
       repeat: 11,
-      setScale: {x: 0.6, y: 0.6 },
-      setXY: { x:400, y: 300 }
+      setScale: {x: 0.5, y: 0.5 },
+      setXY: { x: 425, y: 85 }
     });
 
     balls.children.iterate(function (child) {
@@ -75,7 +88,6 @@ export default new Phaser.Class({
   },
   update: function () {
     const { velocity, x, y } = box.body;
-    console.log(box);
     // if (cursors.space.isDown) {
     //   const x = decelerate(velocity.x);
     //   const y = decelerate(velocity.y);
